419d1075e2a39e2ca290594a1628d4a73536b573
Aldo Medina <u202018286@upc.edu.pe>
2023-04-30 21:19:32 -0500

    Delete success snack bar added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { HomeComponent } from './components/home/home.component';
 import { OffersTableComponent } from './components/offers-table/offers-table.component';
 import {HttpClientModule} from "@angular/common/http";
 import {NgOptimizedImage} from "@angular/common";
+import { AddOfferDialogComponent } from './components/add-offer-dialog/add-offer-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    OffersTableComponent
+    OffersTableComponent,
+    AddOfferDialogComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/offers-table/offers-table.component.ts
+++ src/app/components/offers-table/offers-table.component.ts
@@ -6,6 +6,9 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {OffersDataService} from "../../services/offers-data.service";
 import * as _ from "lodash";
+import {AddOfferDialogComponent} from "../add-offer-dialog/add-offer-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-offers-table',
@@ -18,12 +21,17 @@ export class OffersTableComponent {
   dataSource = new MatTableDataSource();
   displayedColumns: string[] = ['id', 'title', 'description', 'points', 'businessId', 'actions']
 
-  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
   isEditMode = false;
-
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild('offerForm', {static: false}) offerForm!: NgForm;
 
-  constructor(private offersDataService: OffersDataService) {
+  constructor(
+    private offersDataService: OffersDataService,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar
+  )
+  {
     this.offerData = { } as Offer;
   }
 
@@ -33,10 +41,79 @@ export class OffersTableComponent {
     this.getAllOffers();
   }
 
+  onSubmit() {
+    if (this.offerForm.form.valid) {
+      console.log('valid');
+      if (this.isEditMode) {
+        console.log('about to update');
+        this.updateOffer();
+      }
+      else {
+        console.log('about to create');
+        this.addOffer();
+      }
+      this.cancelEditOffer();
+    }
+    else {
+      console.log('Invalid data');
+    }
+  }
+
   getAllOffers() {
     this.offersDataService.getItemList().subscribe((response: any) => {
       this.dataSource.data = response;
     })
+  }
+
+  addOffer() {
+    this.offerData.id = 0;
+    this.offersDataService.createItem(this.offerData).subscribe((response: any) => {
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map((o: any) => { return o})
+    })
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddOfferDialogComponent, {width: '400px', data: { }});
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    })
+  }
+
+  updateOffer() {
+    this.offersDataService.updateItem(this.offerData.id, this.offerData).subscribe((response: any) => {
+      this.dataSource.data = this.dataSource.data.map((o: any) => {
+        if (o.id == response.id) {
+          o = response;
+        }
+        return 0;
+      })
+    })
+  }
+
+  editOffer(element: any) {
+    this.offerData = _.cloneDeep(element);
+    this.isEditMode = true;
+  }
+
+  cancelEditOffer() {
+    this.isEditMode = false;
+    this.offerForm.resetForm();
+  }
+
+  deleteOffer(id: string) {
+    this.offersDataService.deleteItem(id).subscribe((response: any) => {
+      this.dataSource.data = this.dataSource.data.filter((o: any) => {
+        return o.id !== id ? o : false;
+      })
+    })
+    console.log(this.dataSource.data)
+
+    this.snackBar.open('Offer deleted successfully!', 'Close',
+      {
+        duration: 3000,
+      });
   }
 
 
